eaf4f6703a01f480311fa903c804bb81a6b2dbdb
Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2022-05-31 13:12:48 -0300

    Aulas reservas

---
+++ Aula04/Aula04.cs
@@ -0,0 +1,11 @@
+using System; 
+class Aula04{
+
+   static int num = 10;
+
+    static void Main(){
+          int num2 = 15;
+          Console.WriteLine(num);
+    }
+
+}
---
+++ Aula05/Aula05.cs
@@ -0,0 +1,21 @@
+
+// Classes e Operadores Matematicos
+
+using System;
+
+class Aula05 {
+
+
+    static void Main() {
+
+          //& = AND / E
+          // | = OR / OU 
+
+          bool res = (5>3) & (10<5);
+
+          Console.WriteLine(res);
+    }
+
+}
+
+
